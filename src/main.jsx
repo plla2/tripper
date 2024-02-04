e089fe43c96d8edfad47ee34dedd473a3651a2fa
plla2 <jhbljs92@naver.com>
2024-02-05 00:53:53 +0900

    tanstack 활용 데이터불러오기 예제만들기

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const client = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </QueryClientProvider>
+);
