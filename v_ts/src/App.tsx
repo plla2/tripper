215fc9c69f7b35e97fe914109b020edd244addec
plla2 <jhbljs92@naver.com>
2024-02-05 12:34:07 +0900

    라우터, provider 설정

--- v_ts/src/App.tsx
+++ v_ts/src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/header/Header";
+import Main from "./pages/main/Main";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
