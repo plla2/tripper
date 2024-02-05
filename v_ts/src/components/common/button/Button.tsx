ae364dbcf28a7ceab98883a62a2979211521d938
plla2 <jhbljs92@naver.com>
2024-02-05 12:33:41 +0900

    Button 공통 컴포넌트

---
+++ v_ts/src/components/common/button/Button.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+type ButtonTypes = React.DetailedHTMLProps<
+  React.ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+interface ButtonProps extends ButtonTypes {
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    { children, onClick, className, ...props },
+    ref?: React.Ref<HTMLButtonElement>
+  ) => {
+    const classNames =
+      [className, "default-btn"].filter((v) => Boolean(v)).join("") ||
+      undefined;
+    return (
+      <button ref={ref} className={classNames} onClick={onClick} {...props}>
+        {children}
+      </button>
+    );
+  }
+);
+
+export default Button;
