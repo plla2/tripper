5cf4763eb60bd299fb6c728c4768d817e0f3ffb7
plla2 <jhbljs92@naver.com>
2024-02-05 20:21:42 +0900

    scss로 변경, theme 추가

--- v_ts/src/main.tsx
+++ v_ts/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./index.scss";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
