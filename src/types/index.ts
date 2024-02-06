828424b7bd6d0c7d09b21eb6d749a8b8de50b7eb
plla2 <jhbljs92@naver.com>
2024-02-06 22:17:20 +0900

    type코드 분리

---
+++ src/types/index.ts
@@ -0,0 +1,16 @@
+export type FormData = {
+  id: string;
+  password: string;
+};
+
+export interface FormProps {
+  setIsLogin: React.Dispatch<React.SetStateAction<boolean>>;
+  loginType: boolean;
+  setLoginType: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export interface HeaderProps {
+  isLogin: boolean;
+  setIsLogin: React.Dispatch<React.SetStateAction<boolean>>;
+  setLoginType: React.Dispatch<React.SetStateAction<boolean>>;
+}
