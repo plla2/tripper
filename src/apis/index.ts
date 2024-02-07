6bbe314f92279c95f797959f49156a9859c034e2
plla2 <jhbljs92@naver.com>
2024-02-08 01:16:32 +0900

    getMainSliderItems api로직 수정

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-export const getMainItems = async () => {
+export const getMainSliderItems = async () => {
   try {
     const res = await axios.get(
       `${import.meta.env.VITE_API_URL}/searchKeyword1?serviceKey=${
         import.meta.env.VITE_API_KEY
-      }&pageNo=1&numOfRows=30&MobileApp=tripper&MobileOS=ETC&keyword=추천&_type=json`
+      }&pageNo=1&numOfRows=30&MobileApp=tripper&MobileOS=ETC&keyword=코스&_type=json&arrange=O&contentTypeId=25`
     );
-    return res.data.response.body.items.item;
+    return res.data.response.body.items.item.slice(0, 5);
   } catch (error) {
     console.log(error);
   }
