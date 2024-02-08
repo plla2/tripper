aaafe1f1b595d88e58ad5f134c80d0866ddb5dd9
plla2 <jhbljs92@naver.com>
2024-02-08 13:47:16 +0900

    TabPanel 컴포넌트

---
+++ src/components/tabpannel/TabPanel.tsx
@@ -0,0 +1,27 @@
+import { Box, Typography } from "@mui/material";
+
+interface TabPanelType {
+  children?: React.ReactNode;
+  value: number;
+  index: number;
+}
+
+const TabPanel = ({ children, value, index, ...other }: TabPanelType) => {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+export default TabPanel;
