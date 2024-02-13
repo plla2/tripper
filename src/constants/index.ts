87e63579d21e157f83cf93936fe0b16ddfb5f153
plla2 <jhbljs92@naver.com>
2024-02-13 19:22:13 +0900

    location 상수데이터 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,21 @@
+import busanIcon from "../assets/localIcons/busan.webp";
+import cheonbukIcon from "../assets/localIcons/cheonbuk.webp";
+import cheonnamIcon from "../assets/localIcons/cheonnam.webp";
+import chungbukIcon from "../assets/localIcons/chungbuk.webp";
+import chungnamIcon from "../assets/localIcons/chungnam.webp";
+import daeguIcon from "../assets/localIcons/daegu.webp";
+import daejeonIcon from "../assets/localIcons/daejeon.webp";
+import gwangjuIcon from "../assets/localIcons/gwangju.webp";
+import gyeongbukIcon from "../assets/localIcons/gyeongbuk.webp";
+import gyeongnamIcon from "../assets/localIcons/gyeongnam.webp";
+import gyeonggiIcon from "../assets/localIcons/gyeonggi.webp";
+import incheonIcon from "../assets/localIcons/incheon.webp";
+import jejuIcon from "../assets/localIcons/jeju.webp";
+import kangwonIcon from "../assets/localIcons/kangwon.webp";
+import sejongIcon from "../assets/localIcons/sejong.webp";
+import seoulIcon from "../assets/localIcons/seoul.webp";
+import ulsanIcon from "../assets/localIcons/ulsan.webp";
+
 export const HEADER_LIST = [
   {
     id: 1,
@@ -46,5 +64,110 @@ export const FOOTER_LIST = [
     id: 2,
     text: "BLOG",
     url: "https://jhbljs92.tistory.com/",
+  },
+];
+
+export const LOCATION_CODE = [
+  {
+    id: 0,
+    name: "서울",
+    code: "1",
+    img: seoulIcon,
+  },
+  {
+    id: 1,
+    name: "인천",
+    code: "2",
+    img: incheonIcon,
+  },
+  {
+    id: 2,
+    name: "대전",
+    code: "3",
+    img: daejeonIcon,
+  },
+  {
+    id: 3,
+    name: "대구",
+    code: "4",
+    img: daeguIcon,
+  },
+  {
+    id: 4,
+    name: "광주",
+    code: "5",
+    img: gwangjuIcon,
+  },
+  {
+    id: 5,
+    name: "부산",
+    code: "6",
+    img: busanIcon,
+  },
+  {
+    id: 6,
+    name: "울산",
+    code: "7",
+    img: ulsanIcon,
+  },
+  {
+    id: 7,
+    name: "세종",
+    code: "8",
+    img: sejongIcon,
+  },
+  {
+    id: 8,
+    name: "경기",
+    code: "31",
+    img: gyeonggiIcon,
+  },
+  {
+    id: 9,
+    name: "강원",
+    code: "32",
+    img: kangwonIcon,
+  },
+  {
+    id: 10,
+    name: "충북",
+    code: "33",
+    img: chungbukIcon,
+  },
+  {
+    id: 11,
+    name: "충남",
+    code: "34",
+    img: chungnamIcon,
+  },
+  {
+    id: 12,
+    name: "경북",
+    code: "35",
+    img: gyeongbukIcon,
+  },
+  {
+    id: 13,
+    name: "경남",
+    code: "36",
+    img: gyeongnamIcon,
+  },
+  {
+    id: 14,
+    name: "전북",
+    code: "37",
+    img: cheonbukIcon,
+  },
+  {
+    id: 15,
+    name: "전남",
+    code: "38",
+    img: cheonnamIcon,
+  },
+  {
+    id: 16,
+    name: "제주",
+    code: "39",
+    img: jejuIcon,
   },
 ];
