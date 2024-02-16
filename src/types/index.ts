5f2580570e2948b6c371c1cfb3cb00d44094179f
plla2 <jhbljs92@naver.com>
2024-02-16 18:22:11 +0900

    FourCardsGroup에 사용된 props 타입 지정

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,3 +20,31 @@ export interface itemType {
   firstimage: string;
   title: string;
 }
+
+export interface LocalItemsType {
+  data: {
+    addr1: string;
+    addr2: string;
+    areacode: string;
+    booktour: string;
+    cat1: string;
+    cat2: string;
+    cat3: string;
+    contentid: string;
+    contenttypeid: string;
+    cpyrhtDivCd: string;
+    createdtime: string;
+    firstimage: string;
+    firstimage2: string;
+    mapx: string;
+    mapy: string;
+    mlevel: string;
+    modifiedtime: string;
+    sigungucode: string;
+    tel: string;
+    title: string;
+    zipcode: string;
+  }[];
+  header: string;
+  headerTitle: string;
+}
