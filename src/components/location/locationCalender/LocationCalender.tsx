a3c991b9e4bcb924fc4a0457ef13503556d385ae
plla2 <jhbljs92@naver.com>
2024-02-18 15:14:03 +0900

    date 변경시마다 리패치

--- src/components/location/locationCalender/LocationCalender.tsx
+++ src/components/location/locationCalender/LocationCalender.tsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import { ko } from "date-fns/locale";
 import dayjs from "dayjs";
 import { useQuery } from "@tanstack/react-query";
 import { getMainFestivalItems } from "../../../apis";
+import { LOCATION_CODE } from "../../../constants";
+import FiveCardsGroup from "../../common/fiveCardsGroup/FiveCardsGroup";
+import "react-datepicker/dist/react-datepicker.css";
+import "./locationCalender.scss";
 
 type PropsType = {
   currentLocation: number;
@@ -14,22 +17,37 @@ const LocationCalender = ({ currentLocation }: PropsType) => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
   const dayDate = dayjs(selectedDate);
   const formattedDate = dayDate.format("YYYYMMDD");
+  const locationName = LOCATION_CODE.find(
+    (item) => item.code === String(currentLocation)
+  );
   const getLocalFestival = useQuery({
-    queryKey: ["localFestival", currentLocation],
+    queryKey: ["localFestival", currentLocation, formattedDate],
     queryFn: () =>
       getMainFestivalItems(Number(formattedDate), 5, currentLocation),
   });
-  console.log(getLocalFestival?.data);
+
   return (
-    <div>
-      <DatePicker
-        showIcon
-        toggleCalendarOnIconClick
-        selected={selectedDate}
-        onChange={(date) => setSelectedDate(date)}
-        locale={ko}
-      />
-    </div>
+    <>
+      <div className="calender-container">
+        <div className="calender-header">
+          <span className="header-local">{`${locationName?.name}`}</span>
+          <span className="header-content">
+            {" "}
+            의 이런 축제, 행사는 어떠신가요?
+          </span>
+        </div>
+        <DatePicker
+          showIcon
+          toggleCalendarOnIconClick
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
+          locale={ko}
+        />
+      </div>
+      <div className="group-container">
+        <FiveCardsGroup data={getLocalFestival?.data} />
+      </div>
+    </>
   );
 };
 
