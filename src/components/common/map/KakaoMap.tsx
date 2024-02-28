0dace23a902b6e74ca043158593eba8254ec0b8e
plla2 <jhbljs92@naver.com>
2024-02-28 15:02:42 +0900

    받은 데이터를 통한 클러스터, 마커 만들기

--- src/components/common/map/KakaoMap.tsx
+++ src/components/common/map/KakaoMap.tsx
@@ -1,16 +1,32 @@
 import useKakaoLoader from "../../../hooks/useKakaoLoader";
-import { Map } from "react-kakao-maps-sdk";
+import { Map, MarkerClusterer } from "react-kakao-maps-sdk";
+import { LocalItemsType } from "../../../types";
+import MapEventMarker from "../mapEventMarker/MapEventMarker";
 
-const KakaoMap = () => {
+const KakaoMap = ({ data }: LocalItemsType) => {
   useKakaoLoader();
-
   return (
-    <Map
-      id="map"
-      center={{ lat: 33.450701, lng: 126.570667 }}
-      style={{ width: "100%", height: "450px" }}
-      level={3}
-    />
+    <>
+      {data && (
+        <Map
+          id="map"
+          center={{ lat: 36.034, lng: 128.0134 }}
+          style={{
+            width: "60%",
+            height: "450px",
+            display: "flex",
+            justifyContent: "center",
+          }}
+          level={13}
+        >
+          <MarkerClusterer averageCenter={true} minLevel={10}>
+            {data.map((pos) => (
+              <MapEventMarker data={pos} key={pos.contentid} />
+            ))}
+          </MarkerClusterer>
+        </Map>
+      )}
+    </>
   );
 };
 
