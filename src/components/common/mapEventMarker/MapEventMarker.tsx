aeae8e53dad0ca338f39c27a44686c86b4498ba0
plla2 <jhbljs92@naver.com>
2024-02-28 14:59:24 +0900

    마커로 사용될 컴포넌트 MapEventMarker

---
+++ src/components/common/mapEventMarker/MapEventMarker.tsx
@@ -0,0 +1,42 @@
+import { MapMarker } from "react-kakao-maps-sdk";
+import { LocalItemsType } from "../../../types";
+import { useState } from "react";
+
+const MapEventMarker = ({ data }: { data: LocalItemsType["data"][number] }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <>
+      {data && (
+        <MapMarker
+          key={data.contentid}
+          position={{ lat: Number(data.mapy), lng: Number(data.mapx) }}
+          image={{
+            src: data.firstimage2,
+            size: { width: 40, height: 40 },
+          }}
+          onMouseOver={() => setIsVisible(true)}
+          onMouseOut={() => setIsVisible(false)}
+        >
+          {isVisible && (
+            <div
+              style={{
+                color: "#000",
+                fontSize: "16px",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                whiteSpace: "nowrap",
+                padding: "8px",
+                textAlign: "center",
+              }}
+            >
+              {data.title}
+            </div>
+          )}
+        </MapMarker>
+      )}
+    </>
+  );
+};
+
+export default MapEventMarker;
