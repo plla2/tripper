1a0f572c5142389c02a025ca568d4d2e43b5f77d
plla2 <jhbljs92@naver.com>
2024-03-04 21:59:03 +0900

    sliderItem 타입지정

--- src/types/index.ts
+++ src/types/index.ts
@@ -78,3 +78,29 @@ export interface MapMarkerItemType {
     title: string;
   };
 }
+
+export interface SliderItemsType {
+  data: {
+    addr1: string;
+    addr2: string;
+    areacode: string;
+    booktour: string;
+    cat1: string;
+    cat2: string;
+    cat3: string;
+    contentid: string;
+    contenttypeid: string;
+    createdtime: string;
+    firstimage: string;
+    firstimage2: string;
+    cpyrhtDivCd: string;
+    mapx: string;
+    mapy: string;
+    mlevel: string;
+    modifiedtime: string;
+    sigungucode: string;
+    tel: string;
+    title: string;
+  }[];
+  title?: string;
+}
